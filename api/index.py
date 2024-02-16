fe0e58e10edf80b5090ebd0f8310b41281c19297
syntaxerror <142847239+syntaxerror019@users.noreply.github.com>
2024-02-15 21:19:50 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 # This is just the server to host the site. basically nothing cool is here.
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/<url>')
+def home(url):
+    if url is not None:
+        return redirect("https://github.com/syntaxerror019/axumite")
     return render_template("index.html")
 
 @app.route('/economy')
